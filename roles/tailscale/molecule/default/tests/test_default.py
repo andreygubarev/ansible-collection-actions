f99460c7af5c60e0dae55c12b25461030fbe0335
Andrey Gubarev <andrey@andreygubarev.com>
2024-05-17 16:26:49 +0400

    expanded .gitignore, removed kubernetes .gitignore and .yamllint, added readme for kubernetes and tailscale roles, adjusted tailscale tests and tasks, removed with_headscale scenario

--- roles/tailscale/molecule/default/tests/test_default.py
+++ roles/tailscale/molecule/default/tests/test_default.py
@@ -4,10 +4,3 @@
 def test_tailscale_is_installed(host):
     """Check if tailscale is installed."""
     assert host.package("tailscale").is_installed
-
-
-def test_tailscale_running_and_enabled(host):
-    """Check if tailscale is running and enabled."""
-    tailscale = host.service("tailscaled")
-    assert tailscale.is_running
-    assert tailscale.is_enabled
